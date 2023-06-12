93a3716cf06adce7f277e0fcd7d94008a53089e0
JuanFe <jmartinezbernal02@gmail.com>
2023-06-11 23:44:33 -0500

    Jugando con gráficos

--- 02.Yahoo_Finance.py
+++ 02.Yahoo_Finance.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 import streamlit as st
 
@@ -21,7 +22,7 @@ except:
     tickerDF = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
     tickerDF.to_csv('./Data/yahoo_finance.csv', index=False)
 
-
+# First Row
 col1, col2 = st.columns([1, 1])
 
 col1.subheader('Volume')
@@ -29,3 +30,40 @@ col1.line_chart(tickerDF.Volume)
 
 col2.subheader('Close Price')
 col2.line_chart(tickerDF.Close)
+
+# Second Row
+st.write('''
+    #### **Gross Composition**
+''')
+col1, col2, col3 = st.columns([1,0.5,4])
+
+chart_data = pd.DataFrame(
+    np.random.randn(1, 2),
+    columns=["Good Quality", "Bad Quality"])
+
+col1.bar_chart(chart_data)
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 2),
+    columns=['Good Quality', 'Bad Quality']
+)
+
+col3.line_chart(chart_data)
+
+# Third Row
+st.write('''
+    #### **Churn Composition**
+''')
+col1, col2, col3 = st.columns([4,0.5,1])
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 2),
+    columns=['Good Quality', 'Bad Quality']
+)
+col1.line_chart(chart_data)
+
+chart_data = pd.DataFrame(
+    np.random.randn(1, 2),
+    columns=["Good Quality", "Bad Quality"])
+
+col3.bar_chart(chart_data)
