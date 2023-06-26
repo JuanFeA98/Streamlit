a56833bc8862116bcbab9f94f2c1130b1d9f0fe9
JuanFe <jmartinezbernal02@gmail.com>
2023-06-25 23:05:49 -0500

    Preparando archivo para la autenticación

--- Projects/07.Authenticarion_Database/app.py
+++ Projects/07.Authenticarion_Database/app.py
@@ -3,28 +3,3 @@ import numpy as np
 
 import streamlit as st
 import streamlit_authenticator as stauth
-
-from utils.functions import call_data, initialize_firebase
-
-initialize_firebase()
-
-config = call_data()
-
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
-
-name, authentication_status, username = authenticator.login('Login', 'main')
-
-if authentication_status:
-    authenticator.logout('Logout', 'main')
-    st.write(f'Welcome *{name}*')
-    st.title('Some content')
-elif authentication_status == False:
-    st.error('Username/password is incorrect')
-elif authentication_status == None:
-    st.warning('Please enter your username and password')
