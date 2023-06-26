f80ed45f341b65e8b013f841943eb4095463592f
JuanFe <jmartinezbernal02@gmail.com>
2023-06-25 23:51:22 -0500

    Trabajando en la nueva autenticación

--- Projects/07.Authenticarion_Database/app.py
+++ Projects/07.Authenticarion_Database/app.py
@@ -3,3 +3,19 @@ import numpy as np
 
 import streamlit as st
 import streamlit_authenticator as stauth
+
+import requests
+
+url = 'https://auth_demo-1-k6090383.deta.app/credentials/jmart'
+
+response = requests.get(url)
+print(response.json()['credentials'])
+
+
+# authenticator = stauth.Authenticate(
+#     config['credentials'],
+#     config['cookie']['name'],
+#     config['cookie']['key'],
+#     config['cookie']['expiry_days'],
+#     config['preauthorized']
+# )
