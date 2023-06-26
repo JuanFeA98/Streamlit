e4e6fc9f17f2f4ac77c2132044f9f91648342773
JuanFe <jmartinezbernal02@gmail.com>
2023-06-25 23:55:04 -0500

    Autenticación con base de datos: Exitosa

--- Projects/07.Authenticarion_Database/app.py
+++ Projects/07.Authenticarion_Database/app.py
@@ -6,16 +6,32 @@ import streamlit_authenticator as stauth
 
 import requests
 
+import yaml
+from yaml.loader import SafeLoader
+with open('./config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
 url = 'https://auth_demo-1-k6090383.deta.app/credentials/jmart'
-
 response = requests.get(url)
-print(response.json()['credentials'])
 
+config = response.json()
+config = config['message']
 
-# authenticator = stauth.Authenticate(
-#     config['credentials'],
-#     config['cookie']['name'],
-#     config['cookie']['key'],
-#     config['cookie']['expiry_days'],
-#     config['preauthorized']
-# )
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{name}*')
+    st.title('Some content')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
